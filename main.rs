0b3bd7417b7c689faeacd918de2fa4e0c4f8458a
cryptobox <pray,happy76@gmail.com>
2022-09-04 10:57:09 +0900

    initial commit

--- main.rs
+++ main.rs
@@ -1,23 +1,5 @@
-use std::time::Instant;
+mod is_prime;
 
 fn main() {
-    let start = Instant::now();
-    println!("{}", is_prime(100000007));
-    let end = start.elapsed();
-    println!("{}ms", end.subsec_nanos() / 1_000_000);
-}
-
-fn is_prime(num: u32) -> bool {
-    if num == 1 {
-        return false;
-    } else if num == 2 {
-        return true;
-    } else {
-        for i in 2..num {
-            if num % i == 0 {
-                return false;
-            }
-        }
-        true
-    }
+    is_prime::run();
 }
